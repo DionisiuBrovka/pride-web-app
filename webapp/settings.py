8cc7a976934cc4037ee11288cede23a197ebdd39
GIOme3c <giome3c@yandex.ru>
2021-03-16 10:10:18 +0300

    skobochki

--- webapp/settings.py
+++ webapp/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
