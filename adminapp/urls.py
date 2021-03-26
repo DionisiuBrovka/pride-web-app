1622b226dd7f6f12c2e0a977ce84c41ddaf6202e
Dionisiu Brovka <dev.dionisiu.brovka@gmail.com>
2021-03-26 21:53:29 +0300

    admine site chng

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name="adminapp"),
     path('users/', views.pg_users, name = "users"),
-    path('places/', views.pg_places, name = "places")
+    path('user/<int:pk>/', views.pg_user, name="user"),
+    path('places/', views.pg_places, name = "places"),
 ]
--- adminapp/views.py
+++ adminapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import auth
-from data.models import Profile, Place
+from data.models import Profile, Place, Session
 from data.forms import ProfileForm
 
 # Create your views here.
@@ -14,10 +14,28 @@ def pg_users(request):
         return redirect('autherror')
 
     data = {
+        'user':user,
         'users':Profile.objects.all(),
         'form':ProfileForm(),
     }
     return render(request, 'adminapp/pg_users.html', data)
+
+def pg_user(request, pk=1):
+    user = request.user
+
+    if not user.is_authenticated:
+        return redirect('autherror')
+
+    selectUser = Profile.objects.get(id=pk)
+    selectUserSessions = Session.objects.filter(staff__id=pk)
+
+    data = {
+        'user':user,
+        'selectUser':selectUser,
+        'selectUserSessions':selectUserSessions,
+    }
+
+    return render(request, 'adminapp/pg_user.html', data)
 
 def pg_places(request):
     user = request.user
